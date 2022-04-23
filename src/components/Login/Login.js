870172d20e4636f0514c60357cec337405bdab42
bartashb <bartash.b@northeastern.edu>
2022-04-23 15:09:41 -0400

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import { Col, Card, Row, Container } from 'react-bootstrap';
 import './Login.css'
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 //import { Link } from "react-router-dom";
 import MMEAlogo from './MMEA_Logo_Gold.svg';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 function Login() {
 
@@ -15,6 +16,20 @@ function Login() {
     let path = '../Register'; 
     navigate(path);
   }
+
+  //Function that creates JSON body to submit to MONGODB server  
+  const [loginData, setLoginData] = useState({
+    userEmail: "",
+    userPassword: "",
+});
+
+//Uses Axios library to post JSON info from submitScoresheet to MongoDB server. 
+const handleSubmit = e => {
+//Stops page from refreshing
+e.preventDefault();
+axios.post(`http://localhost:5001/login/add`, loginData)
+.then(res =>console.log(res.data))      
+};
 
     return (
         <Container>
@@ -31,15 +46,15 @@ function Login() {
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email"
-                            onChange={(e) => setRegData({...regData, userEmail: e.target.value})} 
-                            value={regData.userEmail} />
+                            onChange={(e) => setLoginData({...loginData, userEmail: e.target.value})} 
+                            value={loginData.userEmail} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password"
-                            onChange={(e) => setRegData({...regData, userPassword: e.target.value})} 
-                            value={regData.userPassword} />
+                            onChange={(e) => setLoginData({...loginData, userPassword: e.target.value})} 
+                            value={loginData.loginPassword} />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Remember me" />
