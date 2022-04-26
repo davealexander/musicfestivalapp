8495fff6d886547b26f27047ea6173241ba5abbb
bartashb <bartash.b@northeastern.edu>
2022-04-26 12:58:55 -0400

    Working on updating fields from database

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 // import 'bootstrap/dist/css/bootstrap.min.css'
-import { Card, Container } from 'react-bootstrap';
-import { Row, Col } from 'react-bootstrap';
+import { Form, Container, Row, Col } from 'react-bootstrap';
 import './MyProfile.css'
-import Form from 'react-bootstrap/Form';
 import Button from '@material-ui/core/Button';
-import MMEAlogo from './MMEA_Logo_Gold.svg';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 function MyProfile() {
 
@@ -16,6 +14,29 @@ function MyProfile() {
     navigate(path);
   }
 
+   //state variable for submitting data to MongoDB
+   const [regData, setRegData] = useState({
+    firstName: "",
+    lastName: "",
+    userEmail: "",
+    userPassword: "",
+    MMEANumber: "",
+    school: "",
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    phoneNumber: "",
+ });
+
+  const [posts, setPost] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5001/register/62620cf2293866d93d449d68').then((response) => {setPost(response.data); });
+   },
+   []);
+
     return (
         <Container>
             <Row>
@@ -24,17 +45,17 @@ function MyProfile() {
                     <Button onClick={routeChange} variant="primary" type="submit" style={{backgroundColor: "rgb(253,182,20)"}}>
                         Edit
                     </Button></Form.Label>
-                    <p>Name: Bailee Bartash</p>
-                    <p>Email: bartash.b@northeastern.edu</p>
-                    <p>Password: ***********</p>
-                    <p>MMEA Number: 123456789</p>
-                    <p>School: John Bapst Memorial High School</p>
-                    <p>Address Line 1: 100 Broadway</p>
-                    <p>Address Line 2:</p>
-                    <p>City: Bangor</p>
-                    <p>State: ME</p>
-                    <p>Zip Code: 04401</p>
-                    <p>Phone Number: 2078056207</p>
+                    <p>Name: {regData.firstName} {regData.lastName}</p>
+                    <p>Email: {regData.userEmail}</p>
+                    <p>Password: {regData.userPassword}</p>
+                    <p>MMEA Number: {regData.MMEANumber}</p>
+                    <p>School: {regData.school}</p>
+                    <p>Address Line 1: {regData.address1}</p>
+                    <p>Address Line 2: {regData.address2}</p>
+                    <p>City: {regData.city}</p>
+                    <p>State: {regData.state}</p>
+                    <p>Zip Code: {regData.zipcode}</p>
+                    <p>Phone Number: {regData.phoneNumber}</p>
                 </div>
             </Row>
         </Container>
